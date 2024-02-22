3b748e5a98573ab1ed9a53002e46f7bd9d8be1e9
baragu <baraguantonyy@gmail.com>
2024-02-22 19:58:36 +0300

    dele

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,5 +31,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	for (i = 0; i < index && *head != NULL; i++)
-		*
+		*head = (*head)->next;
 
+	if (*head == NULL)
+	{
+		*head = saved_head;
+		return (-1); // Index out of bounds
+	}
+
+	tmp = *head;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	tmp->prev->next = tmp->next;
+
+	free(tmp);
+
+	*head = saved_head;
+
+	return (1);
+}
